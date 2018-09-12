ae0235aec7f97517dc8429bfa7090ea69f423a47
mirko <mirko.djurdjevic@brezasoftware.com>
2018-09-12 21:17:53 +0200

    Show only DeliveryOrderItems for that DeliveryOrder

--- src/main/webapp/app/entities/delivery-order-item/delivery-order-item.component.ts
+++ src/main/webapp/app/entities/delivery-order-item/delivery-order-item.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Subscription } from 'rxjs';
-import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
-
-import { IDeliveryOrderItem } from 'app/shared/model/delivery-order-item.model';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Principal } from 'app/core';
+import { IDeliveryOrderItem } from 'app/shared/model/delivery-order-item.model';
+import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
+import { Subscription } from 'rxjs';
+
 import { DeliveryOrderItemService } from './delivery-order-item.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'jhi-delivery-order-item',
     templateUrl: './delivery-order-item.component.html'
 })
 export class DeliveryOrderItemComponent implements OnInit, OnDestroy {
+
+    deliveryOrderId: number;
     deliveryOrderItems: IDeliveryOrderItem[];
+
     currentAccount: any;
     eventSubscriber: Subscription;
 
@@ -20,39 +24,49 @@ export class DeliveryOrderItemComponent implements OnInit, OnDestroy {
         private deliveryOrderItemService: DeliveryOrderItemService,
         private jhiAlertService: JhiAlertService,
         private eventManager: JhiEventManager,
-        private principal: Principal
-    ) {}
-
-    loadAll() {
-        this.deliveryOrderItemService.query().subscribe(
-            (res: HttpResponse<IDeliveryOrderItem[]>) => {
-                this.deliveryOrderItems = res.body;
-            },
-            (res: HttpErrorResponse) => this.onError(res.message)
-        );
-    }
+        private principal: Principal,
+        private route: ActivatedRoute
+    ) { }
 
     ngOnInit() {
+        console.log('test DeliveryOrderItem ngOnInit() ran');
         this.loadAll();
+
         this.principal.identity().then(account => {
             this.currentAccount = account;
         });
         this.registerChangeInDeliveryOrderItems();
     }
 
-    ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
-    }
+    loadAll() {
+        this.route.params.subscribe(params => {
+            this.deliveryOrderId = params['id'];
+        });
 
-    trackId(index: number, item: IDeliveryOrderItem) {
-        return item.id;
+        this.deliveryOrderItemService.findByDeliveryOrderId(this.deliveryOrderId).subscribe(
+            (res: HttpResponse<IDeliveryOrderItem[]>) => {
+                console.log('test DeliveryOrderItem loadAll() response:', res.body);
+                this.deliveryOrderItems = res.body;
+            },
+            (res: HttpErrorResponse) => console.log('test DeliveryOrderItem loadAll() response:', res)
+        );
     }
 
     registerChangeInDeliveryOrderItems() {
         this.eventSubscriber = this.eventManager.subscribe('deliveryOrderItemListModification', response => this.loadAll());
     }
 
+    trackId(index: number, item: IDeliveryOrderItem) {
+        return item.id;
+    }
+
     private onError(errorMessage: string) {
         this.jhiAlertService.error(errorMessage, null, null);
     }
+
+    ngOnDestroy() {
+        console.log('test DeliveryOrderItem ngOnDestroy() ran');
+        this.eventManager.destroy(this.eventSubscriber);
+    }
+
 }
